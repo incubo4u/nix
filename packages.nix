db43072d51daf1afae0b073288a634d77457cc99
incubo4u <52492193+incubo4u@users.noreply.github.com>
2025-06-20 14:44:41 +0200

    Draft home manager

---
+++ packages.nix
@@ -0,0 +1,37 @@
+
+{ pkgs }:
+{
+
+  editors = with pkgs; [ neovim vim ];
+
+  languages = with pkgs; [ cargo go lua python3 rustc zig ];
+
+  networkingTools = with pkgs; [ curl nmap wget wireshark ];
+
+  macOsUtils = with pkgs; [ rectangle ];
+
+  systemUtilities = with pkgs; [
+    atuin
+    bat
+    carapace
+    git
+    ripgrep
+    tmux
+    tree
+    zsh
+  ];
+
+  guiApplications = with pkgs; [
+    anki-bin
+    docker
+    karabiner-elements
+    obsidian
+  ];
+
+  broken = with pkgs; [
+    ghostty
+    ipython
+    keepassxc
+    syncthing
+  ];
+}
