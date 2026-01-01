1b20d6cdbb921965ac8ac744dd88f404a5b1c9cd
cat <cat@dog.fish>
2026-01-01 21:02:42 +0100

    niri

--- home.nix
+++ home.nix
@@ -80,37 +80,24 @@ in {
 
   programs.fzf.enable = true;
   programs.carapace.enable = true;
-  programs.niri =  {
-    enable = true;
 
-    settings = {
-      input = {
-        keyboard = {
-          xkb = {
-            options = "caps:escape";
-          };
-        };
+home.file = {
+  ".config/niri/config.toml" = {
+    text = ''
+      [input]
+      keyboard = "caps:escape"
+      touchpad = "tap=true, natural-scroll=true, dwt=true"
 
-        touchpad = {
-          tap = true;
-          natural-scroll = true;
-          dwt = true; # disable while typing
-        };
-      };
-
-      binds = {
-        "Alt+h" = "focus-column-left";
-        "Alt+l" = "focus-column-right";
-        "Alt+j" = "focus-window-down";
-        "Alt+k" = "focus-window-up";
-
-        "Alt+Return" = {
-          action = "spawn";
-          command = [ "ghostty" ];
-        };
-      };
-    };
+      [binds]
+      "Alt+h" = "focus-column-left"
+      "Alt+l" = "focus-column-right"
+      "Alt+j" = "focus-window-down"
+      "Alt+k" = "focus-window-up"
+      "Alt+Return" = { action = "spawn", command = "ghostty" }
+    '';
   };
+};
+
  
   #### Syncthing (user service) ####
   services.syncthing = {
